812a7521150a516c8fb312c7a86d17ec2cee40aa
hassanbangash2908-beep <hassanbangash2908@gmail.com>
2026-07-29 07:10:09 +0500

    fix: export default app directly in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,5 @@
 import app, { connectDB } from "../server/app.js";
 
-export default async function handler(req, res) {
-  try {
-    await connectDB();
-    return app(req, res);
-  } catch (error) {
-    console.error("Vercel Serverless Function Error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Serverless execution error: " + (error.message || "Internal Error"),
-    });
-  }
-}
+connectDB().catch((err) => console.error("Initial DB connect error:", err.message));
+
+export default app;
